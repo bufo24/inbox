675767cb89820084c2d11e9e75e2395ab89387bc
Jesse van Papenrecht <jessevanpapenrecht@quicknet.nl>
2021-09-27 09:56:01 +0200

    move to jest and updated deployment

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Inbox = await ethers.getContractFactory("Inbox");
+  const inbox = await Inbox.deploy("Hello, Bufo!");
+
+  console.log("Inbox address:", inbox.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,23 +1,22 @@
-const { expect } = require("chai");
+const { describe, expect, test } = require("@jest/globals");
 const { ethers } = require("hardhat");
 
 let inbox;
-
 beforeEach(async () => {
   const Inbox = await ethers.getContractFactory("Inbox");
   inbox = await Inbox.deploy("Hello, world!");
 });
 
 describe("Inbox", () => {
-  it("Deploy Inbox", async function () {
-    expect(inbox).to.exist;
+  test("Deploy Inbox", async () => {
+    expect(inbox).toBeDefined();
     // expect(await inbox.message()).to.be.a;
   });
-  it("Get message", async () => {
-    expect(await inbox.message()).to.equal("Hello, world!");
+  test("Get message", async () => {
+    expect(await inbox.message()).toBe("Hello, world!");
   });
-  it("Change message", async () => {
+  test("Change message", async () => {
     const setMessageTx = await inbox.setMessage("Bye, world!");
-    expect(await inbox.message()).to.equal("Bye, world!");
+    expect(await inbox.message()).toBe("Bye, world!");
   });
 });
